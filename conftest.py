80a1d20a3f4ca10197ce135b486bf11d1ad2fd2f
valsky4 <valentinstoyanov474@gmail.com>
2024-06-26 21:45:21 +0300

    Hello world

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
-import pytest
-
-
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args):
-    return {
-        **browser_context_args,
-        "record_video_dir": "test-reports/videos/"
-    }
+# import pytest
+#
+#
+# @pytest.fixture(scope="session")
+# def browser_context_args(browser_context_args):
+#     return {
+#         **browser_context_args,
+#         "record_video_dir": "test-reports/videos/"
+#     }
