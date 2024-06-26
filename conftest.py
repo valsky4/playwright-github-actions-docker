1a4106eae429b30cb1597ca3945aa1fa5a35e68e
valsky4 <valentinstoyanov474@gmail.com>
2024-06-26 21:06:29 +0300

    Hello world

--- conftest.py
+++ conftest.py
@@ -1,21 +1,9 @@
 import pytest
 
 
-# @pytest.fixture(scope="session")
-# def browser_context_args(browser_context_args):
-#     return {
-#         **browser_context_args,
-#         "viewport": {
-#             "width": 1080,
-#             "height": 768,
-#         }
-#     }
-
-
-# @pytest.fixture(scope="session")
-# def browser_context_args(browser_context_args, playwright):
-#     iphone_11 = playwright.devices['iPhone 11 Pro']
-#     return {
-#         **browser_context_args,
-#         **iphone_11,
-#     }
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "record_video_dir": "test-reports/videos/"
+    }
