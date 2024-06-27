6eb7fc7b4c9c475f957e02102eebefc151c1e135
valsky4 <valentinstoyanov474@gmail.com>
2024-06-27 16:31:55 +0300

    Hello world

--- tests/test_playwright.py
+++ tests/test_playwright.py
@@ -14,3 +14,10 @@ def test_get_started_link(page: Page):
     playwright_page.navigate()
     playwright_page.click_button(role='link', name='Get started')
     playwright_page.assert_page_role_to_be_visible(role='heading', name='Installatio32n')
+
+
+def test_abra_cadabra(page: Page):
+    playwright_page = PlaywrightPage(page)
+    playwright_page.navigate()
+    playwright_page.click_button(role='link', name='Get started')
+    playwright_page.assert_page_role_to_be_visible(role='heading', name='Installatio32n')
