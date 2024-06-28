26463e442b942f7b38070ba0a5a01c3f00bfab10
valsky4 <valentinstoyanov474@gmail.com>
2024-06-28 09:38:52 +0300

    Hello world

--- tests/e2e/test_fe_playwright.py
+++ tests/e2e/test_fe_playwright.py
@@ -1,14 +1,17 @@
+import pytest
 from playwright.sync_api import Page
 
 from pages.playwright_page import PlaywrightPage
 
 
+@pytest.mark.web
 def test_has_title(page: Page):
     playwright_page = PlaywrightPage(page)
     playwright_page.navigate()
     playwright_page.assert_title_contains("Playwright")
 
 
+@pytest.mark.web
 def test_get_started_link(page: Page):
     playwright_page = PlaywrightPage(page)
     playwright_page.navigate()
@@ -16,6 +19,7 @@ def test_get_started_link(page: Page):
     playwright_page.assert_page_role_to_be_visible(role='heading', name='Installatio32n')
 
 
+@pytest.mark.web
 def test_abra_cadabra(page: Page):
     playwright_page = PlaywrightPage(page)
     playwright_page.navigate()
